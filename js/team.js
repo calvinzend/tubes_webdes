48a9af0179608f4851755ac5d6eafff1392f3571
Magic9904 <calvinzend99@gmail.com>
2023-10-20 20:14:53 +0700

    ganti

--- js/team.js
+++ js/team.js
@@ -26,3 +26,22 @@ function nextSlide() {
 tampilkanGambar(index);
 
 setInterval(nextSlide, 3000);
+
+
+
+window.addEventListener("scroll", function() {
+    let lineup = document.querySelector(".lineup");
+    let fullteam = document.querySelector(".fullteam");
+    let muncul = lineup.getBoundingClientRect().top;
+    let muncul2 = fullteam.getBoundingClientRect().top;
+
+
+    if (muncul < window.innerHeight) {
+      lineup.style.transform = "translateY(0)";
+      lineup.style.opacity = 1; 
+    }
+    if (muncul2 < window.innerHeight) {
+      fullteam.style.transform = "translateY(0)";
+      fullteam.style.opacity = 1; 
+    }
+  });
